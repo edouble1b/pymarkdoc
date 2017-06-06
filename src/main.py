861a5967f0b78d39c6b104a80b13b0c306fd740f
Elijah Elmasri <edouble@jeditemple.com>
2017-06-05 18:57:38 -0700

    feat: Now will build the tree create the doc folder with the markdown files

--- src/main.py
+++ src/main.py
@@ -1,12 +1,26 @@
 """Script to create documentation in markdown"""
 import sys
 import doc_tree
+import doc_write
 
 
 def main():
     """Main entry point for the script."""
-    pass
-    print(doc_tree.create_json("/Users/edouble/Documents/Trade_Federation"))
+
+    root_path = "../tests"
+    doc_path = root_path + "/docs"
+
+    tree = doc_tree.create_json(root_path)
+
+    doc_tree.build(tree, doc_path)
+
+    for file in tree:
+        name = file.split("/")[-1]
+        root_file = open("{0}{1}.py".format(root_path, file), "r")
+        doc_file = open("{0}{1}.md".format(doc_path, file), "a")
+
+        #doc_write.intro(name, doc_file)
+        doc_write.modules(name, doc_file, root_file)
 
 
 if __name__ == '__main__':
